966074e8343978e892b774d8f610b5cea0bdb773
박준영 <pjy93525@gmail.com>
2022-09-27 18:35:05 +0900

    image, titleLogo 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img src="/leftImage.png" alt="1" />
+    </div>
+  );
 }
 
 export default App;
