54e9da02ea06a4df4b825e7f36a41bf25ed1f722
박준영 <pjy93525@gmail.com>
2022-10-03 16:20:29 +0900

    ECCR-22 <퍼블리싱 완료>

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import bookMarkBtn from '../../assets/images/Bookmark.png';
+import commentBtn from '../../assets/images/Comment.png';
 import exampleDogImage from '../../assets/images/dog.png';
+import heartBtn from '../../assets/images/Heart.png';
 
 const CardContainer = styled.div`
   display: flex;
@@ -12,15 +15,18 @@ const CardContainer = styled.div`
   border: 1px solid #e8e8e8;
   border-radius: 30px;
   background-color: #fff;
+  padding-bottom: 20px;
 `;
 
 const HeaderContainer = styled.div`
-  display: flex;
-  align-items: center;
   padding: 9px;
   width: 100%;
   height: 55px;
   border-bottom: 1px solid #e8e8e8;
+`;
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
 `;
 const UserImage = styled.div`
   margin-right: 12px;
@@ -44,31 +50,81 @@ const CardImage = styled.div`
   background-size: contain;
   background-repeat: no-repeat;
 `;
-const ButtonContainer = styled.div``;
-const HeartBtn = styled.div``;
-const CommentBtn = styled.div``;
-const ShareBtn = styled.div``;
-const TextContainer = styled.div``;
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px 10px 20px;
+`;
+const HeartCommentContainer = styled.div``;
+const HeartBtn = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 15px;
+`;
+const CommentBtn = styled.img`
+  width: 25px;
+  height: 25px;
+`;
+const BookMarkBtn = styled.img`
+  width: 27px;
+  height: 29px;
+`;
+const HeartCount = styled.div`
+  font-size: 14px;
+  font-weight: bold;
+  padding-left: 20px;
+  padding-bottom: 10px;
+`;
+const CommentCount = styled.div`
+  font-size: 14px;
+  color: #8e8e8e;
+  padding-left: 20px;
+  padding-bottom: 8px;
+`;
+const TextContainer = styled.div`
+  font-weight: 400;
+  font-size: 14px;
+  padding: 0 20px 8px 20px;
+`;
 const CardText = styled.div``;
+const Date = styled.div`
+  font-size: 10px;
+  color: #8e8e8e;
+  padding-right: 20px;
+  text-align: right;
+`;
+
 function Card() {
   return (
     <CardContainer>
       <HeaderContainer>
-        <UserImage />
-        <UserNickName>HELLOLOCA</UserNickName>
+        <UserContainer>
+          <UserImage />
+          <UserNickName>HELLOLOCA</UserNickName>
+        </UserContainer>
       </HeaderContainer>
 
       <CardImage />
 
       <ButtonContainer>
-        <HeartBtn />
-        <CommentBtn />
-        <ShareBtn />
-      </ButtonContainer>
+        <HeartCommentContainer>
+          <HeartBtn src={heartBtn} />
+          <CommentBtn src={commentBtn} />
+        </HeartCommentContainer>
 
+        <BookMarkBtn src={bookMarkBtn} />
+      </ButtonContainer>
+      <HeartCount>좋아요 621개</HeartCount>
       <TextContainer>
-        <CardText>우리 멍멍이 참 귀엽죠 ?</CardText>
+        <UserNickName>HELLOLOCA</UserNickName>
+        <CardText>
+          반갑습니다 여러분 안녕하세요 하이하이 우리 멍멍이 참 귀엽죠 ? 더많은
+          사진을 보고싶으면 저를 팔로우 해주세요 !!
+        </CardText>
       </TextContainer>
+      <CommentCount>댓글 11개</CommentCount>
+      <Date>2022년 2월 2일</Date>
     </CardContainer>
   );
 }
