356a6469f6f47959ed4fa83faa8ce7e7ea154574
박준영 <pjy93525@gmail.com>
2022-10-05 00:09:21 +0900

    ECCR-45 <헤더 하단 고정 수정, z-index 설정 픽스 완료

--- src/components/BottomNav/index.tsx
+++ src/components/BottomNav/index.tsx
@@ -4,10 +4,14 @@ import SearchFeedBtn from '../common/SearchFeedBtn';
 import UploadBtn from '../common/UploadBtn';
 
 const Container = styled.div`
+  position: fixed;
+  z-index: 99;
+  background-color: #fff;
   bottom: 0;
   display: flex;
   align-items: center;
   justify-content: space-around;
+  max-width: 500px;
   width: 100%;
   height: 50px;
   border-top: 1px solid rgb(223, 227, 232);
