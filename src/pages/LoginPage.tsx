08cdeb05cc4c34bac7f8b7acbca16d436723cc19
trevor1107 <qnrudwns123@naver.com>
2022-10-10 15:29:19 +0900

    Revert "Merge pull request #21 from LOCA525/ECCR-52-login-join-setting-page-refactoring"
    This reverts commit 75d801112c66ed04a6464349a1a49336469648fa, reversing
    changes made to fbb1e7ed95baa52e8d521a1b660226d07de9b634.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,41 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import LoginForm from '../components/Account/LoginForm';
-import LoginNavigation from '../components/Account/LoginNavigation';
-import TitleLogo from '../components/TitleLogo';
+import titleLogoImg from '../assets/images/titleLogo.png';
 
 function LoginPage() {
+  const [id, setId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return (
     <Container>
-      <TitleLogo style={{ margin: '4rem' }} />
-      <LoginForm />
-      <LoginNavigation />
+      <TitleLogo>
+        <img src={titleLogoImg} alt="" />
+      </TitleLogo>
+      <Form>
+        <Input
+          required
+          type="email"
+          value={id}
+          placeholder="이메일"
+          onChange={event => setId(event.currentTarget.value)}
+        />
+        <Input
+          required
+          type="password"
+          value={password}
+          placeholder="비밀번호"
+          onChange={event => setPassword(event.currentTarget.value)}
+        />
+        <CheckboxContainer>
+          <SaveIdCheckbox type="checkbox" id="SaveIdCheckbox" />
+          <SaveIdCheckLabel htmlFor="SaveIdCheckbox">
+            아이디 저장
+          </SaveIdCheckLabel>
+        </CheckboxContainer>
+        <LoginBtn>로그인</LoginBtn>
+      </Form>
+      <JoinLink to="/join">회원가입</JoinLink>
     </Container>
   );
 }
@@ -19,5 +46,77 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 7rem;
+  margin-top: 5rem;
+`;
+
+const TitleLogo = styled.div`
+  margin: 4rem 0 4rem 0;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  padding: 0 1.5rem 0 1.5rem;
+`;
+
+const Input = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  height: 3.3rem;
+  padding: 0 1rem 0 1rem;
+  margin-bottom: 0.5rem;
+  background: #fff5eb;
+  border: 1px solid #e3e3e3;
+  border-radius: 10px;
+  caret-color: #ff7f00;
+`;
+
+const CheckboxContainer = styled.div`
+  display: flex;
+  align-self: flex-start;
+  align-items: center;
+  margin-left: 0.2rem;
+`;
+
+const SaveIdCheckLabel = styled.label`
+  font-size: 0.8rem;
+  color: gray;
+
+  :hover {
+    color: black;
+  }
+`;
+
+const SaveIdCheckbox = styled.input`
+  margin-right: 0.3rem;
+  width: 1rem;
+  height: 1rem;
+  accent-color: #ff7f00;
+`;
+
+const LoginBtn = styled.button`
+  margin-top: 1rem;
+  width: 100%;
+  height: 50px;
+  background: #ff7f00;
+  border-radius: 10px;
+
+  font-size: 1rem;
+  font-weight: bold;
+  line-height: 23px;
+  text-align: center;
+  color: #ffffff;
+
+  :hover {
+    background: #fca650;
+    cursor: pointer;
+  }
+`;
+
+const JoinLink = styled(Link)`
+  font-size: 0.9rem;
+  margin-top: 1rem;
 `;
