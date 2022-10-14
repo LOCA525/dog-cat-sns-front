ebfd32b30cbbd23fb601f4cf4119f7b0757ef258
trevor1107 <qnrudwns123@naver.com>
2022-10-14 22:54:26 +0900

    feat: api 폴더 axios instance 생성 및 변수명 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3030/api';
+const api = axios.create({
+  baseURL: 'http://localhost:3030/api',
+});
 
-export default axios;
+export default api;
