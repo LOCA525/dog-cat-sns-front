f6269a342fa27bd76560aca45173546fb63d7ed5
trevor1107 <qnrudwns123@naver.com>
2022-10-15 18:02:17 +0900

    feat: ECCR-35 로그인 기능 완료
    - 로그인 화면에서 아이디 저장 체크하여 로그인시 로컬에 유저 아이디 저장
    - 아이디 저장 체크 recoil 전역 상태로 관리, 로컬에 상태 저장

--- src/components/Account/LoginForm.tsx
+++ src/components/Account/LoginForm.tsx
@@ -1,4 +1,10 @@
-import { useState } from 'react';
+import { useState, MouseEvent } from 'react';
+import axios, { type AxiosError, type AxiosResponse } from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import usePostLogin from '../../hooks/usePostLogin';
+import useSetLocalUserId from '../../hooks/useSetLocalUserId';
+import { saveIdCheckboxState } from '../../store/loginChecked';
 import { SignSubmitBtnStyled } from '../../styles/Account/BtnStyles';
 import { SignFormStyled } from '../../styles/Account/FormStyles';
 import { SignInputStyled } from '../../styles/Account/InputStyles';
@@ -6,11 +12,42 @@ import Input from '../common/Input';
 import LoginCheckbox from './LoginCheckbox';
 
 function LoginForm() {
+  const navigate = useNavigate();
+  const mutateLogin = usePostLogin();
+  const saveUserEmail = useSetLocalUserId();
+  const isSaveIdCheck = useRecoilValue(saveIdCheckboxState);
+
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
+  function handleSubmit(event: MouseEvent<HTMLFormElement>) {
+    event.preventDefault();
+    mutateLogin(
+      { email, password },
+      {
+        onSuccess: (response: AxiosResponse<string, unknown>) => {
+          alert(response?.data);
+          if (isSaveIdCheck) {
+            saveUserEmail(email);
+          } else {
+            saveUserEmail('');
+          }
+          navigate('/');
+        },
+        onError: error => {
+          if (axios.isAxiosError(error)) {
+            const { response } = error as AxiosError;
+            alert(response?.data);
+          } else {
+            console.error(error);
+          }
+        },
+      },
+    );
+  }
+
   return (
-    <SignFormStyled>
+    <SignFormStyled onSubmit={handleSubmit}>
       <Input
         type="email"
         required
--- src/components/Account/SaveIdCheckbox.tsx
+++ src/components/Account/SaveIdCheckbox.tsx
@@ -1,17 +1,26 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
-import useGetSavedUserId from '../../hooks/useGetSavedUserId';
+import useGetLocalUserId from '../../hooks/useGetLocalUserId';
+import { saveIdCheckboxState } from '../../store/loginChecked';
 import { CHECKED_SAVE_ID } from '../../types/localStorageKeys';
 import { Props } from './LoginCheckbox';
 
+// 로그인시 아이디 저장 체크박스에 따라 로컬에 저장된 아이디를 가져와 적용시키는 기능입니다.
 function SaveIdCheckbox({ setUserId }: Props) {
   const initChecked = localStorage.getItem(CHECKED_SAVE_ID);
-  const [checkedSaveId, setCheckedSaveId] = useState<boolean>(
-    initChecked !== null && initChecked !== 'false',
-  );
+  const [checkedSaveId, setCheckedSaveId] =
+    useRecoilState<boolean>(saveIdCheckboxState);
+  const getUserId = useGetLocalUserId();
 
-  const saveUserId = useGetSavedUserId();
-  if (checkedSaveId) setUserId(saveUserId);
+  useEffect(() => {
+    return () => {
+      setCheckedSaveId(initChecked !== null && initChecked !== 'false');
+      const savedUserId = getUserId();
+      checkedSaveId && savedUserId && setUserId(savedUserId);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleCheckedSaveIdChange = () => {
     const newState = !checkedSaveId;
