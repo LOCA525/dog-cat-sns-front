232c7faea2abda3c8754e1b746e436e9c493f1d9
trevor1107 <qnrudwns123@naver.com>
2022-10-31 00:25:44 +0900

    feat: 메인 페이지 접근시 로그인이 되어있지않으면 로그인 페이지로 이동

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import axios, { type AxiosError } from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { getAccountData, getBoardApi } from '../api/board';
 import Board from '../components/Board/Board';
@@ -10,15 +12,27 @@ import { loginUserId } from '../store/loginUser';
 function MainPage() {
   const [id, setUserId] = useRecoilState(loginUserId);
   const [card, setCard] = useRecoilState(cardList);
+  const navigate = useNavigate();
+
   const getAccount = async () => {
     try {
       const res = await getAccountData();
       if (res.status === 200) {
         const res = await getAccountData();
         setUserId(res.data.id); ///로그인 조회 api 사용후 id값을 recoilState 저장
+        return true;
       }
     } catch (error) {
       console.log('에러', error);
+
+      // 로그인이 되어 있지 않는 경우 로그인 페이지로 이동
+      if (axios.isAxiosError(error)) {
+        const { response } = error as AxiosError;
+        if (response && response.status === 404) {
+          navigate('/login');
+        }
+      }
+      return false;
     }
   };
   const getBoard = async () => {
