ce40d669fe88d9689460796fbdb26638ba4b77d6
박준영 <pjy93525@gmail.com>
2022-11-06 16:04:40 +0900

    ECCR-63 <헤더 이미지 오류 수정완료>

--- src/components/UserFeeds/index.tsx
+++ src/components/UserFeeds/index.tsx
@@ -6,7 +6,6 @@ function UserFeeds({ userData }: any) {
     const image = item.Photo.url;
     return image;
   }); //부모컴포넌트에서 api 통신으로 받아온 url을 split하여 미리보기url 주소로 바꾸기 위해 쪼갬
-  console.log(urlList);
 
   return (
     <div>
