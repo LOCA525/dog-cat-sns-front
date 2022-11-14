0b284a90753b898efd9b767e6a0ff2d4f6a2cc9c
박준영 <pjy93525@gmail.com>
2022-11-14 16:23:32 +0900

    수정

--- src/components/Header/UserModal.tsx
+++ src/components/Header/UserModal.tsx
@@ -80,7 +80,7 @@ function UserModal({ setModalOpen }: any) {
 const ModalContainer = styled.div<{ buttonColor: string }>`
   position: absolute;
   right: 5px;
-  top: 55px;
+  top: 60px;
   .triangle {
     right: 10px;
     top: -8px;
